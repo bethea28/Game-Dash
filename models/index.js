3f1bcb450f603b5bbd875f63e61a62f20d956fc4
bryan <bryanbethea28@gmail.com>
2017-07-27 03:19:42 -0400

    good

--- models/index.js
+++ models/index.js
@@ -1,18 +1,29 @@
 'use strict';
 
-const fs        = require('fs');
-const path      = require('path');
-const Sequelize = require('sequelize');
-let basename  = path.basename(module.filename);
-const env       = process.env.NODE_ENV || 'development';
-const config    = require(__dirname + '/../config/config.json')[env];
-let db        = {};
-let sequelize;
+var fs        = require('fs');
+var path      = require('path');
+var Sequelize = require('sequelize');
+var basename  = path.basename(module.filename);
+var env       = process.env.NODE_ENV || 'development';
+var config    = require(__dirname + './../config/config.json')[env];
+var db        = {};
 
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable]);
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+      ssl: true,
+    },
+    logging: true, //false
+  });
 } else {
-  sequelize = new Sequelize(config.url, config);
+  var config = require(__dirname + './../config/config.json')[env];
+  if(config.use_env_variable) {
+    var sequelize = new Sequelize(process.env[config.use_env_variable]);
+  } else {
+    var sequelize = new Sequelize(config.database, config.username, config.password, config);
+  }
 }
 
 fs
@@ -21,7 +32,7 @@ fs
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(function(file) {
-    const model = sequelize['import'](path.join(__dirname, file));
+    var model = sequelize['import'](path.join(__dirname, file));
     db[model.name] = model;
   });
 
@@ -31,7 +42,8 @@ Object.keys(db).forEach(function(modelName) {
   }
 });
 
-db.sequelize = sequelize;
+// db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 module.exports = db;
+
